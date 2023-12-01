9453b80a43609ae728478dc12b5e515aad640062
GabrielGCSOARES <126501271+GabrielGCSOARES@users.noreply.github.com>
2023-12-01 00:35:52 -0300

    ATUALIZAÇÃO 0.1
    ATUALIZAÇÃO 0.1

---
+++ eppp/luis/EPA/conexao.php
@@ -0,0 +1,14 @@
+<?php
+	$host = "localhost"; // endereço do servidor
+	$usuario = "root"; // usuário do MySQL
+	$senha = ""; // senha do MySQL
+	$database = "epa"; // nome do banco de dados
+
+	// Cria a conexão
+	$conn = new mysqli($host, $usuario, $senha, $database);
+	
+	// Checa se a conexão foi realizada com sucesso
+	if ($conn->connect_error) {
+	    die("Falha de conexão: " . $conn->connect_error);
+	}
+?>
